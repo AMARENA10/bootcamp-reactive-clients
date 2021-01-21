b4acfe4fd76eda3bbb695f2ae47dac82a466cf1e
soabel <alfredo.benaute@gmail.com>
2021-01-21 18:51:10 -0500

    update

--- src/main/java/com/bootcamp/reactive/clients/web/controller/ClientsController.java
+++ src/main/java/com/bootcamp/reactive/clients/web/controller/ClientsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bootcamp.reactive.clients.domain.dto.ClientDto;
@@ -22,6 +23,13 @@ public class ClientsController {
 	@Autowired
 	private ClientService clientService;
 	
+	
+	@GetMapping("/find")
+	public Mono<ClientDto> find(@RequestParam String id, @RequestParam String name){
+		System.out.println(name);
+		return this.clientService.findById(id);
+	}
+
 	@GetMapping("/{id}")
 	public Mono<ClientDto> findById(@PathVariable("id") String id){
 		return this.clientService.findById(id);
